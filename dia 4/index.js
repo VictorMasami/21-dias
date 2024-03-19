39bae9a4c9d88af0b728da9db393b5a9beafbb5c
Victor Masami <victormasami06@gmail.com>
2024-03-19 10:15:11 -0300

    Colocando em prática if e else

--- dia 4/index.js
+++ dia 4/index.js
@@ -1,12 +1,19 @@
+// Perguntas
 const fome = prompt("você está com fome? (sim/nao)")
-const dinheiro = prompt("você tem dinheiro para comer em seu retaurante favorito? (sim/nao)")
-const restaurante = prompt("seu restaurante está aberto? (sim/nao)") 
+const dinheiro =prompt("você tem dinheiro para comer fora? (sim/nao)")
+const restaurante =prompt("o resturante que você quer está aberto? (sim/nao)")
 
-if (fome === "nao" || dinheiro == "nao") {
-    console.log("hoje a janta será em casa");
-} else if (dinheiro === "sim" && restaurante === "sim") {
-    console.log("hoje o jantar será no seu restaurante favorito!");
-} else{
-    console.log("Peça um delivery!");
+// respostas mostradas no console
+console.log(fome)
+console.log(dinheiro)
+console.log(restaurante)
 
-}
+if(dinheiro === "nao" || fome === "nao"){
+     console.log("hoje a janta será em casa");
+}else if (fome === "sim" && dinheiro === "sim" && restaurante === "nao"){ 
+      console.log("Peça um delivery!");
+}else{
+    console.log("hoje o jantar será no seu restaurante preferido!!")
+} 
+
+
