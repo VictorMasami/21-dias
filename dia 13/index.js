e8f13c3e50d49e9dbdc9e68ad7ed8b84c930dbc9
Victor Masami <vima30012006@gmail.com>
2024-10-23 10:51:07 -0300

    Aprendendo a pratica de objetos

---
+++ dia 13/index.js
@@ -0,0 +1,19 @@
+class Carro {
+    Nome
+    Potencia
+    VelocidadeMaxima
+    Aceleracao
+    constructoctor (Nome, Aceleracao, Potencia, VelocidadeMaxima){
+        this.Nome = Nome
+        this.Aceleracao = Aceleracao
+        this.Potencia = Potencia
+        this.VelocidadeMaxima = VelocidadeMaxima
+        
+    }
+
+    CalcularTempoMedio(distancia){
+        let resultado = distancia / (VelocidadeMaxima / Aceleracao0);
+            return resultado;
+
+    }
+}
